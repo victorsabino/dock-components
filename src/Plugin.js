288d0b525212d51ace9a54a9929c18f1944c5a05
Victor Sabino <victor.sabino@meta.com.br>
2020-06-03 17:28:42 -0300

    install modal

--- src/Plugin.js
+++ src/Plugin.js
@@ -1,17 +1,31 @@
-import Button from "./components/Button/Button.vue";
+import Logo from "./components/Logo/Logo.vue";
 import Badge from "./components/Badge/Badge.vue";
-import ButtonIcon from "./components/ButtonIcon/ButtonIcon.vue";
-import MenuList from "./components/MenuList/MenuList.vue";
-import Select from "./components/Select/Select.vue";
+import Modal from "./components/Modal/Modal.vue";
 import Status from "./components/Status/Status.vue";
+import Button from "./components/Button/Button.vue";
+import Select from "./components/Select/Select.vue";
 import Toggle from "./components/Toggle/Toggle.vue";
 import TopTab from "./components/TopTab/TopTab.vue";
-import CustomTextArea from "./components/CustomTextArea/CustomTextArea.vue";
 import Checkbox from "./components/Checkbox/Checkbox.vue";
-import Logo from "./components/Logo/Logo.vue";
+import MenuList from "./components/MenuList/MenuList.vue";
+import ButtonIcon from "./components/ButtonIcon/ButtonIcon.vue";
+import CustomInput from "./components/CustomInput/CustomInput.vue";
+import CustomTextArea from "./components/CustomTextArea/CustomTextArea.vue";
 
 module.exports = {
   install: function (Vue, options) {
-    Vue.component('vue-clock', Button);
+    Vue.component('dock-components', Logo);
+    Vue.component('dock-components', Modal);
+    Vue.component('dock-components', Badge);
+    Vue.component('dock-components', Select);
+    Vue.component('dock-components', Status);
+    Vue.component('dock-components', Button);
+    Vue.component('dock-components', Toggle);
+    Vue.component('dock-components', TopTab);
+    Vue.component('dock-components', MenuList);
+    Vue.component('dock-components', Checkbox);
+    Vue.component('dock-components', ButtonIcon);
+    Vue.component('dock-components', CustomInput);
+    Vue.component('dock-components', CustomTextArea);
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,25 +1,33 @@
-import Button from "./components/Button/Button.vue";
+import Logo from "./components/Logo/Logo.vue";
 import Badge from "./components/Badge/Badge.vue";
-import ButtonIcon from "./components/ButtonIcon/ButtonIcon.vue";
-import MenuList from "./components/MenuList/MenuList.vue";
-import Select from "./components/Select/Select.vue";
+import Modal from "./components/Modal/Modal.vue";
 import Status from "./components/Status/Status.vue";
+import Button from "./components/Button/Button.vue";
+import Select from "./components/Select/Select.vue";
 import Toggle from "./components/Toggle/Toggle.vue";
 import TopTab from "./components/TopTab/TopTab.vue";
-import CustomTextArea from "./components/CustomTextArea/CustomTextArea.vue";
 import Checkbox from "./components/Checkbox/Checkbox.vue";
-import Logo from "./components/Logo/Logo.vue";
+import MenuList from "./components/MenuList/MenuList.vue";
+import ButtonIcon from "./components/ButtonIcon/ButtonIcon.vue";
+import CustomInput from "./components/CustomInput/CustomInput.vue";
+import CustomTextArea from "./components/CustomTextArea/CustomTextArea.vue";
+import Vue from "vue";
+import VModal from "vue-js-modal";
+
+Vue.use(VModal, { dialog: true, dynamic: true, injectModalsContainer: true });
 
 export {
-  Button,
-  Badge,
-  ButtonIcon,
-  MenuList,
-  Select,
   Logo,
-  Status,
-  Toggle,
+  Modal,
+  Badge,
   TopTab,
+  Toggle,
+  Status,
+  Select,
+  Button,
   Checkbox,
+  MenuList,
+  ButtonIcon,
+  CustomInput,
   CustomTextArea
 };
