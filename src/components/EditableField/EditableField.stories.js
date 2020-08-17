1a4566ea53c915efc0565bfc32a5d6ba28c8b39c
Lucas Estrela <lucaspaimaro.le@gmail.com>
2020-08-17 10:41:55 -0300

    bump version

--- src/components/EditableField/EditableField.stories.js
+++ src/components/EditableField/EditableField.stories.js
@@ -2,6 +2,11 @@ import EditableField from "./EditableField.vue";
 
 export default { title: "EditableField" };
 
+export const EditableFieldDateFormIsToggle = () => ({
+  components: { EditableField },
+  template:
+    "<div style='width: 120px; margin-left: 20px; margin-top:20px'> <EditableField value='texto'> </EditableField> </div>"
+});
 export const EditableFieldDateFormIsToggledTur = () => ({
   components: { EditableField },
   template:
---
+++ src/components/Money/Money.spec.js
@@ -0,0 +1,73 @@
+import { render, screen, fireEvent } from '@testing-library/vue'
+import Money from './Money.vue'
+import wait from 'waait';
+window.MutationObserver = require("mutation-observer");
+
+//Render test
+test('test if icon is rendering', async () => {
+  render(Money);
+
+  expect(screen.queryByTestId('money')).toBeTruthy();
+})
+
+test('test if icon is not rendering', async () => {
+  render(Money)
+
+  expect(screen.queryByTestId('no_money')).toBeFalsy();
+})
+
+// test('test if icon is swap', async () => {
+//   render(Money, { props: { icon: 'swap' } });
+
+//   expect(screen.queryByText('swap')).toBeTruthy();
+// });
+
+// test('test if icon is not swap', async () => {
+//   render(Money, { props: { icon: 'not_s_wap' } });
+
+//   expect(screen.queryByText('swap')).toBeFalsy();
+// })
+
+// test('test if text is rendering', async () => {
+//   render(Money, { props: { text: 'ImAUniqueText' } });
+
+//   expect(screen.queryByText('ImAUniqueText')).toBeTruthy();
+// })
+
+// test('test if onclick is workng', async () => {
+//   const mockCallback = jest.fn(() => { });
+
+//   render(Money, { props: { onClick: mockCallback } });
+//   fireEvent.click(screen.queryByTestId('mainMoney'));
+//   expect(mockCallback).toHaveBeenCalled();
+// })
+
+// test('test if onclick is workng', async () => {
+//   const mockCallback = jest.fn(() => { });
+
+//   render(Money, { props: { onClick: () => { } } });
+//   fireEvent.click(screen.queryByTestId('mainMoney'));
+//   expect(mockCallback).not.toHaveBeenCalled();
+// })
+
+// test('list Money', async () => {
+//   const mockCallback = jest.fn(() => { });
+  
+//   render(Money, {
+//     props: {
+//       text: 'teste',
+//       onClick: mockCallback,
+//       listMoneys: [{
+//         text: "SERVICE",
+//         icon: "add_circle_outline",
+//         MoneyBgcolor: "#7EC6AE",
+//         MoneyShadow: "#579580",
+//         onClick: mockCallback
+//       }]
+//     }
+//   });
+
+//   fireEvent.click(screen.queryByTestId('mainMoney'));
+//   await wait(0);
+//   expect(screen.getByTestId('listMoneys')).toBeTruthy();
+// })
