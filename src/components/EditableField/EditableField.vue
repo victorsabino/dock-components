44943b2bffb384d1f6aa7b09233c855fa01246ca
Lucas Estrela <lucaspaimaro.le@gmail.com>
2020-08-17 11:30:55 -0300

    Simplifica editableField

--- src/components/EditableField/EditableField.vue
+++ src/components/EditableField/EditableField.vue
@@ -1,8 +1,8 @@
 <template>
   <div @click.stop="toggle" class="EditableField">
-    <div v-if="!isToggled" value="value">{{currentValue}}</div>
+    <div v-if="!isToggled" value="value">{{vmodel}}</div>
     <div v-else class="inputWrapper">
-      <input @blur.stop="toggle" v-model="currentValue"/>
+      <input @blur.stop="toggle" v-model="vmodel"/>
     </div>
   </div>
 </template>
@@ -19,14 +19,18 @@ export default {
             type: String
         }
     },
-    data() {
-      return {
-        currentValue: this.value,
+  computed: {
+    vmodel: {
+      get(){
+        return this.value
+      },
+      set(val){
+        this.$emit("changeField", val);
       }
-    },
+    }
+  },
     methods: { 
         toggle: function () {
-          this.$emit("changeField", this.currentValue);
           this.$emit("toggle ", !this.isToggled)
         },
     }
