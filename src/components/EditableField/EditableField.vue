c4afa4f25471e6b2ac54a57bdc112f25f9f197b2
Victor Sabino <victorsabino6@gmail.com>
2020-08-25 03:30:51 +0000

    editableField and badge updates

--- src/components/EditableField/EditableField.vue
+++ src/components/EditableField/EditableField.vue
@@ -1,40 +1,51 @@
 <template>
   <div @click.stop="toggle" class="EditableField">
-    <div v-if="!isToggled" value="value">{{currentValue}}</div>
+    <div v-if="!isToggled" value="value">
+      <Ellipsis :name="currentValue" :width="width"/>
+    </div>
     <div v-else class="inputWrapper">
-      <input @blur.stop="toggle" v-model="currentValue" :maxlength="maxlength"/>
+      <input @blur.stop="toggle" v-model="currentValue" :maxlength="maxlength" />
     </div>
   </div>
 </template>
 
 <script>
+import Ellipsis from "../Ellipsis";
+
 export default {
-    props: {
-        isToggled: {
-            default: false,
-            type: Boolean
-        },
-        value: {
-            default: '',
-            type: String
-        },
-        maxlength: {
-          default: '100',
-          type: String
-        }
+  props: {
+    isToggled: {
+      default: false,
+      type: Boolean,
     },
-    data() {
-      return {
-        currentValue: this.value,
-      }
-    }, 
-    methods: { 
-        toggle: function () {
-          this.$emit("changeField", this.currentValue);
-          this.$emit("toggle ", !this.isToggled)
-        },
-    }
-}
+    value: {
+      default: "",
+      type: String,
+    },
+    maxlength: {
+      default: "100",
+      type: String,
+    },
+     width: {
+      default: "200px",
+      type: String,
+    },
+  },
+  data() {
+    return {
+      currentValue: this.value,
+    };
+  },
+  methods: {
+    toggle: function () {
+      this.$emit("changeField", this.currentValue);
+      this.$emit("toggle ", !this.isToggled);
+    },
+  },
+  components: {
+    Ellipsis,
+  },
+};
 </script>
 
 <style scoped>
