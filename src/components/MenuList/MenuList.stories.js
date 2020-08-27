d3362456a42f28457a4d5b2c3dd32b8e25f7bedb
Lucas Estrela <lucaspaimaro.le@gmail.com>
2020-08-27 16:59:23 -0300

    Atualiza style do menuList

--- src/components/MenuList/MenuList.stories.js
+++ src/components/MenuList/MenuList.stories.js
@@ -1,4 +1,5 @@
 import MenuList from "./MenuList.vue";
+import ButtonIcon from "../ButtonIcon/ButtonIcon.vue";
 
 export default { title: "MenuList" };
 
@@ -13,4 +14,10 @@ export const MenuListStoryWidth = () => ({
   components: { MenuList },
   template:
     "<MenuList width='100px'>"
+});
+
+export const MenuListStoryItems = () => ({
+  components: { MenuList, ButtonIcon },
+  template:
+    "<MenuList width='100px' :items=\"[{ name: 'Edit', onClick: () => {}},{ name: 'See transactions', onClick: () => {}}, { name: 'Delete', onClick: () => {}}]\"> <div slot='button' style='position: relative; height: 10px'> <ButtonIcon icon='more_vert' bgcolor='transparent' color='#103a4f' width='45px' height='90px' /> </div> </MenuList>"
 });
