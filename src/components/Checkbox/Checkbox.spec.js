c474e3e4d1f039495337c5e0de345cd37ba015b5
Lucas Estrela <lucaspaimaro.le@gmail.com>
2020-09-11 13:34:00 -0300

    wip

--- src/components/Checkbox/Checkbox.spec.js
+++ src/components/Checkbox/Checkbox.spec.js
@@ -1,19 +1,27 @@
 import { render, screen, fireEvent } from '@testing-library/vue'
 import Checkbox from './Checkbox.vue'
+import { dockRender } from '../../../test/utils'
+
 window.MutationObserver = require("mutation-observer");
 
+//Render test
 test('test if checkbox is rendering', async () => {
-  render(Checkbox, {props: {icon: 'swap'}});
+  render(Checkbox);
 
   expect(screen.queryByTestId('checkbox')).toBeTruthy();
 })
 
+test('test if checkbox is not rendering', async () => {
+  render(Checkbox)
+
+  expect(screen.queryByTestId('not_checkbox')).toBeFalsy();
+})
+
+//Click test
 test('test if checkbox is clicking', async () => {
-    let vmodel = false;
-    let mock = jest.fn();
-    render(Checkbox, {props: {'v-model': vmodel}, 'v-on': {change: mock}});
-    fireEvent.click(screen.queryByTestId('checkbox'));
-    console.log('LOOOL ', vmodel)
-    expect(mock).toHaveBeenCalled();
+    let { container, rerender } = dockRender(Checkbox, {props: {value: true}});
+    await fireEvent.click(screen.queryByTestId('checkbox').firstChild);
+    console.log(container.innerHTML)
+    console.log(screen.queryByTestId('checkbox').firstChild)
+    expect(screen.queryByTestId('checkbox')).toBeTruthy();
   })
-  
